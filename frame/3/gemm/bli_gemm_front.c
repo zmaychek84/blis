3eb97177922f27d8bfa9376a2946bdee897400dc
Meghana Vankadari <Meghana.Vankadari@amd.com>
2022-01-27 13:01:22 +0530

    Fixed a bug in deriving dimensions from objects in bli_gemm_front.c file
    Change-Id: I4cc38d8f1dee277f99b5532e4645e0e1bc5b31cb

--- frame/3/gemm/bli_gemm_front.c
+++ frame/3/gemm/bli_gemm_front.c
@@ -5,7 +5,7 @@
    libraries.
 
    Copyright (C) 2014, The University of Texas at Austin
-   Copyright (C) 2018 - 2021, Advanced Micro Devices, Inc. All rights reserved.
+   Copyright (C) 2018 - 2022, Advanced Micro Devices, Inc. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -176,7 +176,7 @@ void bli_gemm_front
 	
 	dim_t m_dim_local = bli_obj_length( &c_local );
 	dim_t n_dim_local = bli_obj_width( &c_local );
-	dim_t k_dim_local = bli_obj_width( &a_local );
+	dim_t k_dim_local = bli_obj_width_after_trans( &a_local );
 #ifdef BLIS_CONFIG_EPYC
 	// Regression observed in sgemm native path in cases where m >= 4 * n 
 	// after BLIS_THREAD_RATIO_M updated from 2 to 1 as part of commit 
